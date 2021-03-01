8d9ea028ea14c0aff38ff8bbb39c418691417c46
dream <dream_chou93@163.com>
2021-03-01 13:12:46 +0800

    提交弹框管理框架及示例

---
+++ assets/load/scripts/LoadScene.ts
@@ -0,0 +1,14 @@
+
+import { _decorator, Component, Node, director } from 'cc';
+import { PopupManager } from '../../libs/popup/manager/PopupManager';
+const { ccclass, property } = _decorator;
+
+@ccclass('LoadScene')
+export class LoadScene extends Component {
+
+    start() {
+        PopupManager.instance.init();
+        director.loadScene('PopupTest');
+    }
+}
+
